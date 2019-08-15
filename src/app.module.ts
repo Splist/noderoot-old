5ea1151276df9160a61571cee3bb97bf2ccd89bf
Jdender~ <jdenderplays@gmail.com>
2019-08-15 14:06:01 -0400

    Feat: Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const Typeorm = TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: __dirname + '/../data/splist.sqlite',
+    entities: [__dirname + '/**/?(*.)entity.ts'],
+    synchronize: true,
+});
 
 @Module({
+    imports: [Typeorm],
 })
 export class AppModule {}
