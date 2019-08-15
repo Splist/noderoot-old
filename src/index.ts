707ddf6d0a3611a08dec787bad6c45e5ba5947d5
Jdender~ <jdenderplays@gmail.com>
2019-08-15 14:56:17 -0400

    Feat: Add the ValidationPipe and related dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 void async function() {
 
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({
+        whitelist: true,
+        transform: true,
+    }));
 
     await app.listen(8080);
 }();
