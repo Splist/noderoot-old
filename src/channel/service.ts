f9ee4cea29d168a084e1311b027d03e696a932fc
Jdender~ <jdenderplays@gmail.com>
2019-08-25 16:27:20 -0400

    Feat/Fix: Have ChannelService insure that a root channel exists

--- src/channel/service.ts
+++ src/channel/service.ts
@@ -1,15 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Channel } from './entity';
 import { TreeRepository } from 'typeorm';
+import { ConfigService } from '../config/service';
 
 @Injectable()
 export class ChannelService {
 
+    private readonly logger = new Logger(this.constructor.name);
+
     constructor(
         @InjectRepository(Channel)
         private readonly repo: TreeRepository<Channel>,
-    ) {}
+        private readonly configService: ConfigService,
+    ) {
+        this.ensureRootChannel();
+    }
+
+    async ensureRootChannel() {
+
+        const tree = await this.fetchTree();
+
+        if (tree.length > 0) return;
+
+        this.logger.log('Root channel not found. Creating one...');
+
+        const { config } = this.configService;
+
+        await this.create({
+            internalName: config.internalRootName,
+        });
+
+        this.logger.log('Root channel created.');
+    }
 
     fetchTree() {
         return this.repo.findTrees();
