7b210d4ba1da3f94671f4e67bc0201fa4367a43f
Jdender~ <jdenderplays@gmail.com>
2019-09-08 17:50:03 -0400

    Add RootGraphQLModule

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from './config';
 import { ChannelModule } from './channel';
 
@@ -10,9 +11,23 @@ const RootTypeOrmModule = TypeOrmModule.forRoot({
     synchronize: true,
 });
 
+// [TODO] Add better config for these
+const GENERATE_SCHEMA = false;
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const RootGraphQLModule = GraphQLModule.forRoot({
+
+    // autoSchemaFile must be true or string for it to work
+    autoSchemaFile: GENERATE_SCHEMA ? 'schema.gql' : true,
+
+    // Only in dev
+    playground: !PRODUCTION,
+});
+
 @Module({
     imports: [
         RootTypeOrmModule,
+        RootGraphQLModule,
         ConfigModule,
         ChannelModule,
     ],
