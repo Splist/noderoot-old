4f50d482fd51176347ed2dc3aba235ecd3dd644a
Jdender~ <jdenderplays@gmail.com>
2019-09-14 15:35:40 -0400

    Add CompoundResovler children and createCompound

--- src/channel/compound/resolver.ts
+++ src/channel/compound/resolver.ts
@@ -1,15 +1,23 @@
-import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation, ResolveProperty, Parent } from '@nestjs/graphql';
 import { CompoundService } from './service';
-import { Int } from 'type-graphql';
+import { Int, Arg } from 'type-graphql';
 import { CompoundChannel } from './entity';
 import { RawScalar } from './raw';
+import { CreateChannelInput } from './input';
 
-@Resolver()
+@Resolver(() => CompoundChannel)
 export class CompoundResolver {
 
     constructor(
         private readonly channels: CompoundService,
     ) {}
+
+    @ResolveProperty()
+    children(
+        @Parent() { id }: CompoundChannel
+    ) {
+        return this.channels.fetchChildren(id);
+    }
 
     @Query(returns => CompoundChannel, { nullable: true })
     compoundChannel(
@@ -26,5 +34,12 @@ export class CompoundResolver {
     @Query(returns => CompoundChannel)
     rootChannel() {
         return this.channels.fetchRoot();
+    }
+
+    @Mutation(returns => CompoundChannel)
+    createCompound(
+        @Args('input') input: CreateChannelInput,
+    ) {
+        return this.channels.create(input);
     }
 }
